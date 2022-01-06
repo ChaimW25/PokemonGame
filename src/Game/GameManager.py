56f4c4f4d89e56a2c24fa647443c7a881227d38a
TahelZecharia <tahelest@gmail.com>
2022-01-06 02:00:30 +0200

    algo

--- src/Game/GameManager.py
+++ src/Game/GameManager.py
@@ -1,4 +1,6 @@
 import json
+import math
+import queue
 
 from src.Game.client import Client
 from Pokemon import Pokemon
@@ -12,13 +14,72 @@ class GameManager:
     def __init__(self, client: Client):
 
         self.client = client
-        self.info = None
+        self.info: GameInfo = None
         self.pokemons = []
         self.agents = []
         self.graphAlgo = GraphAlgo()
 
         self.load_info()
         self.load_graph()
+        self.add_agents()
+
+    # the function update the info, the agents list, and the pokemons list.
+    def update(self):
+
+        self.load_info()
+        self.load_agent()
+        self.load_pokemon()
+
+    def allocate_all_agents(self):
+
+        """
+        Go through all the agents, in case one of the agents is at rest,
+        the function sends it to a function that will locate the nearest
+        Pokemon and update the agent's destination accordingly.
+        """
+
+        self.update()
+        for agent in self.agents:
+            if agent.get_dest() == -1:
+                self.allocate_agent_dest(agent)
+
+    def allocate_agent_dest(self, agent: Agent):
+
+        """
+        The function receives an agent and locates the nearest
+        Pokemon and updates the agent's destination accordingly.
+        :param agent:
+        """
+
+        min_dist: float = math.inf
+        dest: int = -1
+
+        for pok in self.pokemons:
+            temp_dist, path = self.graphAlgo.shortest_path(agent.get_src(), pok.get_src())
+            temp_dist = temp_dist / agent.get_speed()
+            if temp_dist < min_dist:
+                min_dist = temp_dist
+                if min_dist == 0:
+                    dest = pok.get_dest()
+                else:
+                    dest = path[1]
+
+        self.client.choose_next_edge('{"agent_id":'+str(agent.get_id())+', "next_node_id":'+str(dest)+'}')
+
+    def add_agents(self):
+
+        self.load_pokemon()
+        q = queue.PriorityQueue()
+
+        for pok in self.pokemons:
+            q.put(pok)
+
+        for i in range(self.info.num_of_agents()):
+            if not q.empty():
+                pok: Pokemon = q.get()
+                self.client.add_agent("{\"id\":" + str(pok.get_src()) + "}")
+            else:
+                self.client.add_agent("{\"id\":" + str(i+1 % self.graphAlgo.get_graph().nodesCounter) + "}")
 
     # loads game info to info
     def load_info(self):
@@ -36,6 +97,7 @@ class GameManager:
     def load_pokemon(self):
 
         json_pokemons = json.loads(self.client.get_pokemons())
+        self.pokemons = []
         for p in json_pokemons["Pokemons"]:
             po = Pokemon(**p["Pokemon"])
             po.set_graph(self.graphAlgo.get_graph())
